0595db314f77d21208bd6535a456f37c2e599ca7
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-22 14:30:07 +0300

    15 - Listing posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="POST">
+            <form action="{{ route('posts') }}" method="POST" class="mb-4">
                 @csrf
 
                 <div class="mb-4">
@@ -25,6 +25,19 @@
                 </div>
 
             </form>
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold">{{ $post->user->name }}</a>
+
+                        <p class="mb-2">{{ $post->body }}</p>
+                        <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+                    </div>
+                @endforeach
+            @else
+                <p>There are no posts yet</p>
+            @endif
         </div>
     </div>
 @endsection
