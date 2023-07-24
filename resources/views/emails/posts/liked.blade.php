b204da40e270aa2d775ce0c5db515190421e2d11
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-24 11:46:23 +0300

    26 - Sending email

---
+++ resources/views/emails/posts/liked.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# You post was liked
+
+{{ $liker->name }} liked one of your posts
+
+<x-mail::button :url=route('posts.show', $post)>
+    View post
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
